d76a6a327452634a3170e03ad112c5e331f1538f
Roman Volkov <bessukabit@gmail.com>
2022-11-27 09:39:53 +0200

    added empty page

--- src/pages/EmptyPage/EmptyPage.tsx
+++ src/pages/EmptyPage/EmptyPage.tsx
@@ -1,7 +1,20 @@
+import { useNavigate } from 'react-router-dom';
+
+import { LargeButton } from 'components/Buttons/LargeButton';
+
 import styled from './EmptyPage.module.scss';
 
 const EmptyPage = () => {
-  return <div className={styled.container}>404 empty page</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className={styled.container}>
+      <p>page not found</p>
+      <div className={styled.btnWrapper} onClick={() => navigate(-1)}>
+        <LargeButton title='Go back' />
+      </div>
+    </div>
+  );
 };
 
 export { EmptyPage };
