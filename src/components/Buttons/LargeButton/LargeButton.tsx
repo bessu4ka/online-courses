173d51c317c9223b83d9a77b4447f4110f2c3c88
Roman Volkov <bessukabit@gmail.com>
2022-11-28 23:23:00 +0200

    add animations

--- src/components/Buttons/LargeButton/LargeButton.tsx
+++ src/components/Buttons/LargeButton/LargeButton.tsx
@@ -5,7 +5,11 @@ import { IButton } from 'types/Button';
 import styled from './LargeButton.module.scss';
 
 const LargeButton: FC<IButton> = ({ title }) => {
-  return <button className={styled.container}>{title}</button>;
+  return (
+    <button data-ripple className={styled.container}>
+      {title}
+    </button>
+  );
 };
 
 export { LargeButton };
