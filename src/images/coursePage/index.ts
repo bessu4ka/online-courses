51e448a8484cd045e0d86cbb8675609a107310a1
Roman Volkov <bessukabit@gmail.com>
2022-11-28 20:12:23 +0200

    curator++ course-details page

---
+++ src/images/coursePage/index.ts
@@ -0,0 +1,5 @@
+import curator from './curator.png';
+import program from './program-illustration.png';
+import testimonials from './testimonials-illustration.png';
+
+export { curator, program, testimonials };
