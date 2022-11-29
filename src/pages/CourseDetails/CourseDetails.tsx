e57b671d8e32b82728f7ccb8d4c2a2eefa1d59c8
Roman Volkov <bessukabit@gmail.com>
2022-11-29 15:16:32 +0200

    program++ course-detail page

--- src/pages/CourseDetails/CourseDetails.tsx
+++ src/pages/CourseDetails/CourseDetails.tsx
@@ -3,6 +3,7 @@ import { CTA } from 'components/CourseDetailsPage/CTA';
 import { Curator } from 'components/CourseDetailsPage/Curator';
 import { ForWhom } from 'components/CourseDetailsPage/ForWhom';
 import { PageTitle } from 'components/CourseDetailsPage/PageTitle';
+import { Program } from 'components/CourseDetailsPage/Program';
 import { Steps } from 'components/CourseDetailsPage/Steps';
 
 import styled from './CourseDetails.module.scss';
@@ -16,6 +17,7 @@ const CourseDetails = () => {
       <Steps />
       <CTA />
       <ForWhom />
+      <Program />
     </main>
   );
 };
