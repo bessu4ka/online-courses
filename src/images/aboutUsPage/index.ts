bb60d27990af6b84a92e08920d20031e43126b0f
Roman Volkov <bessukabit@gmail.com>
2022-12-01 10:29:21 +0200

    add team section for aboutUs page

--- src/images/aboutUsPage/index.ts
+++ src/images/aboutUsPage/index.ts
@@ -6,5 +6,30 @@ import img_3 from './direction_image_3.png';
 import img_4 from './direction_image_4.png';
 import img_5 from './direction_image_5.png';
 import studying from './studying-progress.png';
+import team_1 from './team_1.png';
+import team_2 from './team_2.png';
+import team_3 from './team_3.png';
+import team_4 from './team_4.png';
+import team_5 from './team_5.png';
+import team_6 from './team_6.png';
+import team_7 from './team_7.png';
+import team_8 from './team_8.png';
 
-export { info, statistic, img_1, img_2, img_3, img_4, img_5, studying };
+export {
+  info,
+  statistic,
+  img_1,
+  img_2,
+  img_3,
+  img_4,
+  img_5,
+  studying,
+  team_1,
+  team_2,
+  team_3,
+  team_4,
+  team_5,
+  team_6,
+  team_7,
+  team_8,
+};
--- src/pages/AboutUs/AboutUs.tsx
+++ src/pages/AboutUs/AboutUs.tsx
@@ -1,8 +1,12 @@
 import { Directions } from 'components/AboutUsPage/Directions';
 import { Info } from 'components/AboutUsPage/Info';
 import { StudyingProcess } from 'components/AboutUsPage/StudyingProcess';
+import { Team } from 'components/AboutUsPage/Team';
 import { Values } from 'components/AboutUsPage/Values';
 import { VideoStatistic } from 'components/AboutUsPage/VideoStatistic';
+import { Blog } from 'components/HomePage/Blog';
+import { Subscribe } from 'components/HomePage/Subscribe';
+import { Testimonials } from 'components/HomePage/Testimonials';
 
 import styled from './AboutUs.module.scss';
 
@@ -14,6 +18,10 @@ const AboutUs = () => {
       <Values />
       <Directions />
       <StudyingProcess />
+      <Team />
+      <Testimonials />
+      <Blog />
+      <Subscribe />
     </main>
   );
 };
