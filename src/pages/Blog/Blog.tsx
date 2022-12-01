382304e337c87a20567ab8791a333bc9fba00cb8
Roman Volkov <bessukabit@gmail.com>
2022-12-01 18:57:52 +0200

    create & add subscribe section for blog

--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,10 +1,13 @@
 import { BlogGridMenu } from 'components/BlogPage/BlogGridMenu';
+import { Subscribe } from 'components/BlogPage/Subscribe';
+
 import styled from './Blog.module.scss';
 
 const Blog = () => {
   return (
     <main className={styled.container}>
       <BlogGridMenu />
+      <Subscribe />
     </main>
   );
 };
