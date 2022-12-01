8842a6670c884ae9ee2c6db9451bda5c0331181b
Roman Volkov <bessukabit@gmail.com>
2022-12-01 12:01:21 +0200

    pagination component

---
+++ src/components/BlogPage/BlogPage.tsx
@@ -0,0 +1,13 @@
+import { Pagination } from 'components/Pagination';
+
+import styled from './BlogPage.module.scss';
+
+const BlogPage = () => {
+  return (
+    <section>
+      <Pagination />
+    </section>
+  );
+};
+
+export { BlogPage };
--- src/pages/Blog/Blog.tsx
+++ src/pages/Blog/Blog.tsx
@@ -1,7 +1,12 @@
+import { BlogPage } from 'components/BlogPage';
 import styled from './Blog.module.scss';
 
 const Blog = () => {
-  return <main className={styled.container}>Blog</main>;
+  return (
+    <main className={styled.container}>
+      <BlogPage />
+    </main>
+  );
 };
 
 export { Blog };
