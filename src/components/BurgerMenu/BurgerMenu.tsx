c97745d2d27b83a7146b4475f06855a42800220b
Roman Volkov <bessukabit@gmail.com>
2022-12-08 16:25:02 +0200

    modal menu navigate

---
+++ src/components/BurgerMenu/BurgerMenu.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { routes } from 'utils/routes';
+
+import styled from './BurgerMenu.module.scss';
+
+interface IProps {
+  closeModal: () => void;
+}
+
+const BurgerMenu: FC<IProps> = ({ closeModal }) => {
+  return (
+    <nav className={styled.container}>
+      <ul className={styled.linkWrapper}>
+        <NavLink
+          onClick={closeModal}
+          className={({ isActive }) => (isActive ? styled.activeLink : styled.link)}
+          to='/'>
+          home
+        </NavLink>
+        {routes.map(({ title, link }, index) => {
+          return (
+            <li key={index}>
+              <NavLink
+                onClick={closeModal}
+                className={({ isActive }) => (isActive ? styled.activeLink : styled.link)}
+                to={link}>
+                {title}
+              </NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export { BurgerMenu };
